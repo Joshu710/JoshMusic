45f9dba259e764f3d1d3d802fc0cc5f679e70371
Joshua Bernal <josh@Joe-2.local>
2025-03-23 00:23:06 -0400

    Added MP3 Playback

--- JoshMusic/app/(tabs)/player.tsx
+++ JoshMusic/app/(tabs)/player.tsx
@@ -1,15 +1,49 @@
 import { Text, View, Button, Pressable } from "react-native";
 import { Link } from "expo-router";
-import TrackPlayer from "react-native-track-player";
+import { Audio } from "expo-av";
+import { useEffect, useState } from "react";
+import { Sound } from "expo-av/build/Audio";
 
 export default function Player() {
-  //   function foo() {
-  //     console.log("Button Pressed");
-  //   }
+  const [sound, setSound] = useState<Sound>();
+
+  // Audio.setAudioModeAsync({
+  //   staysActiveInBackground: true,
+  //   playsInSilentModeIOS: true,
+
+  // });
+
+  async function playSound() {
+    console.log("Loading Sound");
+    await Audio.setAudioModeAsync({
+      allowsRecordingIOS: false,
+      staysActiveInBackground: true,
+      playsInSilentModeIOS: true,
+      shouldDuckAndroid: true,
+      playThroughEarpieceAndroid: false,
+    });
+    const { sound } = await Audio.Sound.createAsync(
+      require("@/assets/Hello.mp3")
+    );
+    setSound(sound);
+
+    console.log("Playing Sound");
+    await sound.playAsync();
+  }
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          console.log("Unloading Sound");
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
 
   return (
     <View>
       <Text>Player Page</Text>
+      <Button title="Play Sound" onPress={playSound} />
     </View>
   );
 }
