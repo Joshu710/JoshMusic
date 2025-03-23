1c7f479c7fac8836d82408e381b7fd81e6ee18a7
Joshua Bernal <josh@Joe-2.local>
2025-03-22 22:42:19 -0400

    Starter Tabs

---
+++ JoshMusic/app/(tabs)/player.tsx
@@ -0,0 +1,15 @@
+import { Text, View, Button, Pressable } from "react-native";
+import { Link } from "expo-router";
+import TrackPlayer from "react-native-track-player";
+
+export default function Player() {
+  //   function foo() {
+  //     console.log("Button Pressed");
+  //   }
+
+  return (
+    <View>
+      <Text>Player Page</Text>
+    </View>
+  );
+}
---
+++ JoshMusic/app/(tabs)/url.tsx
@@ -0,0 +1,15 @@
+import { Text, View, Button, Pressable} from "react-native";
+import { Link } from 'expo-router';
+
+export default function URLTest() {
+
+//   function foo() {
+//     console.log("Button Pressed");
+//   } 
+
+  return (
+    <View>
+      <Text>This is the URL Page</Text>
+    </View>
+  );
+}
